979e5728aa18e89203ffc2ec54e680197073028f
hyeleenotharry <dlgpgl1029@gmail.com>
2023-08-08 09:28:37 +0900

    no message

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 app = Flask(__name__)
 from pymongo import MongoClient
 import certifi
@@ -11,6 +11,10 @@ db = client.dbsparta
 @app.route('/')
 def home():
    return render_template('index.html')
+
+@app.route('/webstagram')
+def webstagram():
+   return render_template('instagram.html')
 
 @app.route('/signUp', methods=["POST"])
 def sign_up():
@@ -34,13 +38,13 @@ def sign_in():
    id = db.user.find_one({'id':id_log})
    pwd = db.user.find_one({'pwd':pwd_log})
 
-   #print(id, pwd)
 
    if(id and pwd):
       print("success")
+      redirect(url_for('webstagram'))
       return jsonify({'msg':'로그인 성공!'})
    #다른 페이지로 redirect
-      redirect('/webstagram')
+      
    else:
       print("fail")
       return jsonify({'msg':'사용자를 찾지 못했습니다. 회원가입을 진행해주세요.'})
